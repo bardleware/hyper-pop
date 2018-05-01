9b25746bdff72c4abff53cf132808ceef3a4cfa5
bardleware <code@devansisson.com>
2018-04-30 20:25:41 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# hyper-pop
+Slightly modified `hyper-thirtyone` theme for Hyper.
+
+
+### Install
+
+1. Edit Hyper's preferences (`~/.hyper.js`).
+2. Add the plugin:
+  
+  ```js
+plugins: [
+  'hyper-pop'
+],
+```
+
---
+++ index.js
@@ -0,0 +1,54 @@
+// Constants
+const backgroundColor = `#37312f`;
+const foregroundColor = '#f2f2f2';
+const borderColor = '#faa41a';
+
+// Colors
+const RED = '#F55';
+const GREEN = '#50fa7b';
+const ORANGE = '#ffb86c';
+const LIGHT_ORANGE = '#f1fa8c';
+const BLUE = '#66d9ef';
+const LIGHT_BLUE = '#6FC1FF';
+const PINK = '#ff79c6';
+const LIGHT_PINK = '#FF9AC1';
+const CYAN = '#6FC1FF';
+const PURPLE = '#bd93f9';
+const LIGHT_GRAY = foregroundColor;
+const MEDIUM_GRAY = '#676B79';
+const WHITE = '#FFFFFF';
+
+const colors = {
+    black: backgroundColor,
+    red: RED,
+    green: GREEN,
+    yellow: ORANGE,
+    blue: BLUE,
+    magenta: PINK,
+    cyan: CYAN,
+	  orange: PURPLE,
+    white: LIGHT_GRAY,
+    lightRed: RED,
+    lightGreen: GREEN,
+    lightYellow: LIGHT_ORANGE,
+    lightBlue: LIGHT_BLUE,
+    lightMagenta: LIGHT_PINK,
+    lightCyan: CYAN,
+    lightWhite: WHITE
+};
+
+// Theme config
+exports.decorateConfig = (config) => (
+  Object.assign({}, config, {
+    backgroundColor,
+    foregroundColor,
+    borderColor: borderColor,
+    cursorColor: borderColor,
+    cursorShape: 'UNDERLINE',
+    fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    colors,
+    css: `
+      ${config.css || ''}
+    `
+  })
+);
